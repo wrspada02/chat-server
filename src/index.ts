858ce01bbe672c0d566366ed9ca19f350664d85a
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-03-19 15:50:36 -0300

    chore: adjust imports and add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { roomRoute } from "./routes/room";
+
 import { userRoute } from "./routes/user";
+import { roomRoute } from "./routes/room";
 
 const PORT = 5000;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.use("/room", roomRoute);
-app.use("/user", userRoute);
+app.use("/rooms", roomRoute);
+app.use("/users", userRoute);
 
 httpServer.listen(PORT, () => {
   console.log("Server is running on port 5000");
--- src/routes/room/index.ts
+++ src/routes/room/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+
 import { io } from "../..";
 
 const roomRoute = Router();
