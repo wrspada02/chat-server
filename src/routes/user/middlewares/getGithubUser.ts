99eb782620b54810a5f66a1e5fbb0fc24f8917ca
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-07-03 20:58:51 -0300

    chore: add octokit package following github documentation

--- src/routes/user/middlewares/getGithubUser.ts
+++ src/routes/user/middlewares/getGithubUser.ts
@@ -1,15 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import { IGithubUser } from "../../../modules/user/dto/IGithubUser";
-import axios from "axios";
+import { Octokit } from "@octokit/core";
 
 async function getGithubUser (request: Request, response: Response, next: NextFunction) {
   const token: string = request.params.token;
+  const octokit = new Octokit({ auth: token });
 
-  const githubUser: IGithubUser = await axios.get("https://api.github.com/user", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    }
-  });
+  const githubUser = await octokit.request("GET /user");
 
   if (githubUser) {
     request.body = githubUser;
